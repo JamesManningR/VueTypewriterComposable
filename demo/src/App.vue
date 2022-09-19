e65e524ad2d5d475c47b4e78c62d012d5df74272
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2022-09-19 20:54:14 +0100

    Added replacement example

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -14,9 +14,10 @@ const strings = ref([
   "你好世界 !",
 ])
 
-const breakTheApp = () => {
-  strings.value = ["Eat it"]
-}
+const exampleReplacementStrings = [
+  "Goodbye world!",
+  "Пока Світ!",
+];
 
 const {
   text,
@@ -35,71 +36,113 @@ const {
   isAtLastLetter,
   isLastIteration,
   isPausingAtEnd,
+  finishEmpty,
   pauseAtEndOfWord,
   pause,
-  play
-} = useTypewriter(strings, {
-  typeInterval: 1000,
-});
+  play,
+  safeUpdateStrings
+} = useTypewriter(strings);
 </script>
 
 <template>
 <div class="demo">
   <h2>
-    &nbsp;{{ text }}
+    {{ text || `&nbsp` }}
   </h2>
 
-  <form @submit.prevent class="controls">
-    <button type="button" @click="play" :disabled="!isPaused">Play</button>
-    <button type="button" @click="pause" :disabled="isPaused">Pause</button>
-    <button type="button" @click="pauseAtEndOfWord" :disabled="isPausingAtEnd">Pause at end</button>
-    <button type="button" @click="breakTheApp">Break me</button>
-    <label>
-      Type Interval(ms):
-      <input type="number" min="1" v-model="typeInterval" />
-    </label>
-    <label>
-      Delete Interval(ms):
-      <input type="number" min="1" v-model="deleteInterval" />
-    </label>
-    <label>
-      Hold For(ms):
-      <input type="number" min="1" v-model="holdFor" />
-    </label>
-    <label>
-      Hold Empty For(ms):
-      <input type="number" min="1" v-model="holdEmptyFor" />
-    </label>
-    <label>
-      Loop:
-      <input type="checkbox" v-model="loop" />
-    </label>
-    <label v-if="loop">
-      Iterations:
-      <input type="number" v-model="iterations" />
-    </label>
-  </form>
+  <details @submit.prevent class="controls">
+    <summary>Controls</summary>
+    <fieldset>
+      <h3>Functions</h3>
+      <button type="button" @click="play" :disabled="!isPaused">Play</button>
+      <button type="button" @click="pause" :disabled="isPaused">Pause</button>
+      <button type="button" @click="pauseAtEndOfWord" :disabled="isPausingAtEnd">Pause at end</button>
+      <button type="button" @click="safeUpdateStrings(exampleReplacementStrings)">Safe Replace</button>
+    </fieldset>
 
-  <Debug 
-    :currentString="currentString"
-    :currentAction="currentAction"
-    :typedLength="typedLength"
-    :stringIndex="stringIndex"
-    :iteration="iteration"
-    :strings="strings"
-    :typeInterval="typeInterval"
-    :deleteInterval="deleteInterval"
-    :holdFor="holdFor"
-    :loop="loop"
-    :iterations="iterations"
-    :words="strings"
-    :isPaused="isPaused"
-    :isAtLastLetter="isAtLastLetter"
-    :isLastIteration="isLastIteration"
-    :isPausingAtEnd="isPausingAtEnd"
-    :holdEmptyFor="holdEmptyFor"
-  />
+    <fieldset>
+      <h3>Timing</h3>
+
+      <label>
+        Type Interval(ms):
+        <input type="number" required min="1" v-model="typeInterval" />
+      </label>
+      <label>
+        Delete Interval(ms):
+        <input type="number" required min="1" v-model="deleteInterval" />
+      </label>
+      <label>
+        Hold For(ms):
+        <input type="number" required min="1" v-model="holdFor" />
+      </label>
+      <label>
+        Hold Empty For(ms):
+        <input type="number" required min="1" v-model="holdEmptyFor" />
+      </label>
+    </fieldset>
+
+    <fieldset>
+      <h3>Loop behaviour</h3>
+
+      <label>
+        Loop:
+        <input type="checkbox" v-model="loop" />
+      </label>
+      <label v-if="loop">
+        Iterations:
+        <input type="number" v-model="iterations" />
+      </label>
+    </fieldset>
+  </details>
+
+  <details>
+    <summary>Debug</summary>
+    <Debug 
+      :currentString="currentString"
+      :currentAction="currentAction"
+      :typedLength="typedLength"
+      :stringIndex="stringIndex"
+      :iteration="iteration"
+      :strings="strings"
+      :typeInterval="typeInterval"
+      :deleteInterval="deleteInterval"
+      :holdFor="holdFor"
+      :loop="loop"
+      :iterations="iterations"
+      :words="strings"
+      :isPaused="isPaused"
+      :isAtLastLetter="isAtLastLetter"
+      :isLastIteration="isLastIteration"
+      :isPausingAtEnd="isPausingAtEnd"
+      :holdEmptyFor="holdEmptyFor"
+      :finishEmpty="finishEmpty"
+    />
+  </details>
+
 </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+details {
+  margin-bottom: 1em;
+}
+
+.controls {
+  fieldset {
+    display: flex;
+    gap: 0 1em;
+    flex-wrap: wrap;
+    margin-bottom: 1em;
+
+    h3 {
+      margin-top: 0;
+      width: 100%;
+    }
+
+    label {
+      margin-top: 0;
+      width: calc(25% - .75em);
+    }
+  }
+}
+</style>
--- demo/src/components/Debug.vue
+++ demo/src/components/Debug.vue
@@ -18,6 +18,7 @@ type Props = {
   isAtLastLetter: boolean;
   isLastIteration: boolean;
   isPausingAtEnd: boolean;
+  finishEmpty: boolean;
 }
 
 const props = defineProps<Props>();
@@ -25,104 +26,103 @@ const props = defineProps<Props>();
 
 <template>
   <div class="debug">
+    <blockquote>
+      All of these are reactive, so you can use them in your own components.
+      All options can be changed to adjust the behaviour of the typewriter dynamically.
+    </blockquote>
 
-    <section class="vars">
-      <h2>Reactive Vars</h2>
+    <div class="vars">
+      <section>
+        <h3>Strings in the list</h3>
 
-      <div class="tables">
-        <section>
-          <h3>Strings in the list</h3>
-          
-          <ul>
-            <li v-for="(string, index) in strings" @key="string"
+        <ul>
+          <li v-for="(string, index) in strings" @key="string"
             :class="{ active: index === stringIndex, deleting: index === stringIndex && currentAction === TypewriterState.Deleting }">
             {{ string }}</li>
-          </ul>
-        </section>
+        </ul>
+      </section>
 
-        <section class="state">
-          <h3>State</h3>
-          <table>
-            <tr>
-              <td>Current string</td>
-              <td>{{ currentString }}</td>
-            </tr>
-            <tr>
-              <td>Current Action</td>
-              <td>{{ currentAction }} ({{ TypewriterState[currentAction] }})</td>
-            </tr>
-            <tr>
-              <td>Typed length</td>
-              <td>{{ typedLength }}</td>
-            </tr>
-            <tr>
-              <td>String index</td>
-              <td>{{ stringIndex }}</td>
-            </tr>
-            <tr>
-              <td>Iteration</td>
-              <td>{{ iteration }}</td>
-            </tr>
-            <tr>
-              <td>Is paused</td>
-              <td>{{ isPaused }}</td>
-            </tr>
-            <tr>
-              <td>Is at last letter</td>
-              <td>{{ isAtLastLetter }}</td>
-            </tr>
-            <tr>
-              <td>Is last iteration</td>
-              <td>{{ isLastIteration }}</td>
-            </tr>
-            <tr>
-              <td>Is pausing at end</td>
-              <td>{{ isPausingAtEnd }}</td>
-            </tr>
-          </table>
-        </section>
+      <section class="state">
+        <h3>State</h3>
+        <table>
+          <tr>
+            <td>Current string(*)</td>
+            <td>{{ currentString }}</td>
+          </tr>
+          <tr>
+            <td>Current Action</td>
+            <td>{{ currentAction }} ({{ TypewriterState[currentAction] }})</td>
+          </tr>
+          <tr>
+            <td>Typed length</td>
+            <td>{{ typedLength }}</td>
+          </tr>
+          <tr>
+            <td>String index</td>
+            <td>{{ stringIndex }}</td>
+          </tr>
+          <tr>
+            <td>Iteration</td>
+            <td>{{ iteration }}</td>
+          </tr>
+          <tr>
+            <td>Is paused</td>
+            <td>{{ isPaused }}</td>
+          </tr>
+          <tr>
+            <td>Is at last letter</td>
+            <td>{{ isAtLastLetter }}</td>
+          </tr>
+          <tr>
+            <td>Is last iteration</td>
+            <td>{{ isLastIteration }}</td>
+          </tr>
+          <tr>
+            <td>Is pausing at end</td>
+            <td>{{ isPausingAtEnd }}</td>
+          </tr>
+        </table>
+      </section>
 
-        <section class="options">
-          <h3>Options</h3>
-          <table>
-            <tr>
-              <td>Type interval</td>
-              <td>{{ typeInterval }}</td>
-            </tr>
-            <tr>
-              <td>Delete interval</td>
-              <td>{{ deleteInterval }}</td>
-            </tr>
-            <tr>
-              <td>Hold for</td>
-              <td>{{ holdFor }}</td>
-            </tr>
-            <tr>
-              <td>Hold Empty for</td>
-              <td>{{ holdEmptyFor }}</td>
-            </tr>
-            <tr>
-              <td>Loop</td>
-              <td>{{ loop }}</td>
-            </tr>
-            <tr>
-              <td>Iterations</td>
-              <td>{{ iterations }}</td>
-            </tr>
-          </table>
-        </section>
-      </div>
-    </section>
+      <section class="options">
+        <h3>Options</h3>
+        <table>
+          <tr>
+            <td>Type interval</td>
+            <td>{{ typeInterval }}</td>
+          </tr>
+          <tr>
+            <td>Delete interval</td>
+            <td>{{ deleteInterval }}</td>
+          </tr>
+          <tr>
+            <td>Hold for</td>
+            <td>{{ holdFor }}</td>
+          </tr>
+          <tr>
+            <td>Hold Empty for</td>
+            <td>{{ holdEmptyFor }}</td>
+          </tr>
+          <tr>
+            <td>Loop</td>
+            <td>{{ loop }}</td>
+          </tr>
+          <tr>
+            <td>Iterations</td>
+            <td>{{ iterations }}</td>
+          </tr>
+          <tr>
+            <td>Finish Empty</td>
+            <td>{{ finishEmpty }}</td>
+          </tr>
+        </table>
+      </section>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-.debug {
-  display: flex;
-  gap: 2em;
-}
-
-.tables {
+.vars {
   display: flex;
   gap: 1em;
 }
